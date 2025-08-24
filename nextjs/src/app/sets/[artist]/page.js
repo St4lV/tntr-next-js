1d12399bb035ff3f52d31a2ca9215fcc592a4ccd
st4l <152074070+St4lV@users.noreply.github.com>
2025-08-24 18:06:37 +0200

    NextJS - Artists sets display

--- nextjs/src/app/sets/[artist]/page.js
+++ nextjs/src/app/sets/[artist]/page.js
@@ -9,19 +9,60 @@ export default function BlogPostPage({ params }) {
   const { artist } = use(params)
   const {artists_list} = useGlobalContext();
   let act_artist_obj
-  function isArtistExist(artist_val){
+  const [artist_sets, setArtistSets]= useState([])
+
+  async function isArtistExist(){
     if (artists_list.length ===0){return}
     var found = artists_list.find(artist_obj=>{act_artist_obj=artist_obj;return artist_obj.title_min === decodeURIComponent(artist)})
     if (!found){router.push("/sets");return}
     document.querySelector("#artist-page-artist-name").innerText=act_artist_obj.title
+    await fetchArtistSets();
+  }
+
+  async function fetchArtistSets(){
+    let result;
+    const url = `/ap1/v1/radio/artists/${artist}/sets`
+    try {
+        const response = await fetch(url, {
+        method: "GET", 
+        headers: { "Content-Type": "application/json" }
+    });
+        const data = await response.json();
+        result = data
+        
+    } finally {
+        if (result){
+          setArtistSets(result.log)
+        } else {
+          router.push("/sets");return
+        }
+        
+    }
+  }
+
+  function displayArtistSets(){
+    if (artist_sets.length>0){
+      const artist_sets_div=document.getElementById("artist-sets-holder")
+      let result =""
+      for (let i of artist_sets){
+        result+=`<article class="episodes-comp"><hr><div class="episodes-comp-internal"><img class="episodes-img" src='${i.cover}' alt='${i.title} cover'/><div class="episodes-text"><h3>${i.title}</h3><hr><p>${i.desc}</p></div></div><hr></article>`
+      }
+      artist_sets_div.innerHTML=result
+    }
+    
   }
   useEffect(() => {
-    isArtistExist(artist);
+    isArtistExist();
   }, [artists_list]);
+  useEffect(() => {
+    displayArtistSets();
+  }, [artist_sets]);
+
 
   return (
     <main>
       <h2 id="artist-page-artist-name"></h2>
+      <div id="artist-sets-holder"></div>
     </main>
   )
 }
