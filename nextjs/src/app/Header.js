3dc54a91ce18a73659412f5d461061bad42df264
st4l <152074070+St4lV@users.noreply.github.com>
2025-08-26 11:12:03 +0200

    Side menu integration

--- nextjs/src/app/Header.js
+++ nextjs/src/app/Header.js
@@ -1,18 +1,20 @@
 'use client'
-        
-let menu_opened=false
-import Link from 'next/link'
 
-function BurgerMenu(){
-if (menu_opened){
-    console.log("closing menu")
-    menu_opened=false
-} else {
-    console.log("opening menu")
-    menu_opened=true
-}
-}
+import Link from 'next/link'
+import { useState } from 'react';
+
 export default function Header(){
+
+    const [menu_opened, setMenuOpened] = useState(false);
+    function burgerMenu(){
+        setMenuOpened(!menu_opened)
+        console.log(menu_opened)
+        console.log(document.getElementById("header-side-menu").dataset.opened)
+    }
+    
+    function closeMenu(){
+        setMenuOpened(false)
+    }
 
     return(
         <header>
@@ -20,16 +22,21 @@ export default function Header(){
             <h1>Tirnatek Radio</h1>
             </div>
             <nav className="burger-menu">
-            <svg id="burger-menu-icon" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16" onClick={BurgerMenu}>
-                <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
-            </svg>
-            <ul>
+                <svg onClick={burgerMenu} id="burger-menu-icon" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+                    <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+                </svg>
+                <ul id="header-side-menu" data-opened={menu_opened}>
+                
                 <li>
-                <Link href="/">Diffusion en direct</Link>
+                    <Link href="/" className='header-side-menu-element' onClick={closeMenu}>Diffusion en direct</Link>
+                    <hr/>
                 </li>
+                
                 <li>
-                <Link href="/sets">DJ sets</Link>
+                    <Link href="/sets" className='header-side-menu-element' onClick={closeMenu}>DJ sets</Link>
+                    <hr/>
                 </li>
+                
             </ul>
             </nav>
         </header>
