062fafcda9ce0db18bfbeac7c74d805c4e3cfd8e
st4l <152074070+St4lV@users.noreply.github.com>
2025-09-03 12:08:28 +0200

    Title and meta description addition in layout

--- nextjs/src/app/layout.js
+++ nextjs/src/app/layout.js
@@ -10,9 +10,14 @@ export default function RootLayout({ children }) {
 		<html lang="fr">
 		<head>
 			<meta charSet="UTF-8"/>
+			<title>Tirnatek Radio</title>
+			<meta
+			name="description"
+			content="Tekno Webradio basée en France, écoutez notre sélection de musiques tekno en direct, ainsi que les derniers DJ sets des artistes collaborant avec nous !"
+			/>
 			<link rel="preconnect" href="https://fonts.googleapis.com"/>
 			<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
-			<link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400..900&display=swap" rel="stylesheet"/>
+			<link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@100..900&display=swap" rel="stylesheet"/>
 			<meta name="viewport" content="width=device-width, initial-scale=1"></meta>
 			
 			<meta name="google-site-verification" content="cCnNinz1iS12t7Hg_AY0m9Zsl40wHLpis3uHDfrzH1Y" />
