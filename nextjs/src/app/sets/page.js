b8a662c26b280cf6dc3864e3a3263c3bd5b72913
st4l <152074070+St4lV@users.noreply.github.com>
2025-09-04 11:58:33 +0200

    li in ul + Image + indentation

--- nextjs/src/app/sets/page.js
+++ nextjs/src/app/sets/page.js
@@ -10,43 +10,40 @@ import { useGlobalContext } from "@/app/GlobalContext";
 import Endpage from "@/app/Endpage";
 
 export default function Home() {
-  const { artists_list } = useGlobalContext();
-  const [search, setSearch] = useState("");
+	const { artists_list } = useGlobalContext();
+	const [search, setSearch] = useState("");
 
-  function renderDJsList() {
-    if (!artists_list) return null;
+	function renderDJsList() {
+		if (!artists_list) return null;
 
-    return artists_list
-      .filter((i) =>
-        i.title_min.toLowerCase().includes(search.toLowerCase()) || i.title.toLowerCase().includes(search.toLowerCase())
-      )
-      .map((i) => (
-        <div key={i.title_min}>
-          <li
-            data-artist={i.title_min}
-            className="artist-comp"
-          >
-            <Link href={`/sets/${i.title_min}`}>
-              <h3 className="artist-comp-title">{i.title}</h3>
-			        <hr/>
-              <Image src={i.cover} className="available-djs-img" alt={i.title} height={150} width={150}/>
-              {i.episodes_nb} sets
-              <hr/>
-              <p className="artist-comp-desc">{i.desc_short}</p>
-            </Link>
-          </li>
-        </div>
-      ));
-  }
+		return artists_list
+		.filter((i) =>
+			i.title_min.toLowerCase().includes(search.toLowerCase()) || i.title.toLowerCase().includes(search.toLowerCase())
+		)
+		.map((i) => (
+			<li key={i.title_min} data-artist={i.title_min} className="artist-comp">
+				<Link href={`/sets/${i.title_min}`}>
+				<h3 className="artist-comp-title">{i.title}</h3>
+				<hr/>
+				<center>
+				<Image src={i.cover} className="available-djs-img" alt={i.title} height={150} width={150}/>
+				</center>
+				<p>{i.episodes_nb} sets</p>
+				<hr/>
+				<p className="artist-comp-desc">{i.desc_short}</p>
+				</Link>
+			</li>
+		));
+	}
 
-  return (
-    <main>
-      <div>
-        <h2>DJ sets</h2>
-        <input type="search" placeholder="Artiste, Soundsystem .. " value={search} onChange={(e) => setSearch(e.target.value)}/>
-      </div>
-      <ul id="available-djs">{renderDJsList()}</ul>
-      <Endpage/>
-    </main>
-  );
-}
+	return (
+		<main>
+		<div>
+			<h2>DJ sets</h2>
+			<input type="search" placeholder="Artiste, Soundsystem .. " value={search} onChange={(e) => setSearch(e.target.value)}/>
+		</div>
+		<ul id="available-djs">{renderDJsList()}</ul>
+		<Endpage/>
+		</main>
+	);
+	}
