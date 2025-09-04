9cf2d658639930af44ccb330e93c15dfc639c5a2
st4l <152074070+St4lV@users.noreply.github.com>
2025-09-04 11:16:57 +0200

    Indentation + proxy for now_playing images

--- express/azuracast/routes/main.js
+++ express/azuracast/routes/main.js
@@ -18,46 +18,69 @@ const front_api_link = frontend_values.api_route;
 
 //GET - Actuellement joué | GET à Azuracast
 router.get("/now_playing", async (req, res) => {
-  const response = await getApiRequest(`${azuracast_server}/api/nowplaying_static/${station_shortcode}.json`,req);
-  if (response.code!=200){return res.status(response.code).json(response)};
-  const response_log=response.log
-  let result={
-	station:{
-	  name:response_log.station.name,
-	  description:response_log.station.description,
-	  mounts:[],
-	  listen_url:`${front_api_link}/radio/mountpoints/tntr128.mp3`,
-	},
-	listeners:response_log.listeners,
-	live:response_log.live,
-	now_playing:response_log.now_playing,
-	playing_next:response_log.playing_next,
-	song_history:response_log.song_history
-  }
-  for (let i of response_log.station.mounts){
-	result.station.mounts.push({name:i.name,url:`${front_api_link}/radio/mountpoints/${(i.path).split("/")[1]}`,bitrate:i.bitrate})
-  }
+	const response = await getApiRequest(`${azuracast_server}/api/nowplaying_static/${station_shortcode}.json`,req);
+	if (response.code!=200){return res.status(response.code).json(response)};
+	const response_log=response.log
+	let result={
+		station:{
+		name:response_log.station.name,
+		description:response_log.station.description,
+		mounts:[],
+		listen_url:`${front_api_link}/radio/mountpoints/tntr128.mp3`,
+		},
+		listeners:response_log.listeners,
+		live:response_log.live,
+		now_playing:response_log.now_playing,
+		playing_next:response_log.playing_next,
+		song_history:response_log.song_history
+	}
 
-  res.status(response.code).json({code:response.code,type:response.type,log:result});
+	function getMediaUrl(val){
+		return front_api_link + "/radio/now_playing/cover/" + val.split(`${azuracast_server}/api/station/tntr/art/`)[1];
+	}
+
+	result.now_playing.song.art = getMediaUrl(result.now_playing.song.art);
+	result.playing_next.song.art = getMediaUrl(result.playing_next.song.art);
+
+	for (let i of result.song_history){
+		i.song.art = getMediaUrl(i.song.art);
+	}
+
+	for (let i of response_log.station.mounts){
+		result.station.mounts.push({name:i.name,url:`${front_api_link}/radio/mountpoints/${(i.path).split("/")[1]}`,bitrate:i.bitrate})
+	}
+
+	res.status(response.code).json({code:response.code,type:response.type,log:result});
+});
+
+router.get("/now_playing/cover/:cover", async (req, res) => {
+	try {
+	const { cover } = req.params;
+	await getMediaRequest(`${azuracast_server}/api/station/tntr/art/${cover}`,res,req);
+
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({code: 500,type: "Internal Server Error",log: error.message});
+	}
 });
 
 //POST - Planning | GET à Azuracast
 router.get("/schedule", async (req, res) => {
-  const response = await getApiRequest(`${azuracast_server}/api/station/${station_shortcode}/schedule?now=now&rows=48`, req);
-  return res.status(response.code).json(response);
+	const response = await getApiRequest(`${azuracast_server}/api/station/${station_shortcode}/schedule?now=now&rows=48`, req);
+	return res.status(response.code).json(response);
 });
 
 //GET - Flux audios | GET à Azuracast
 router.get("/mountpoints/:mount", async (req, res) => {
-  try {
-	const { mount } = req.params;
+	try {
+		const { mount } = req.params;
 
-	await getMediaRequest(`${azuracast_server}/listen/tntr/${mount}`,res,req);
+		await getMediaRequest(`${azuracast_server}/listen/tntr/${mount}`,res,req);
 
-  } catch (error) {
-	console.error(error);
-	res.status(500).json({code: 500,type: "Internal Server Error",log: error.message});
-  }
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({code: 500,type: "Internal Server Error",log: error.message});
+	}
 });
 
 ////// ARTISTS ROUTES //////
