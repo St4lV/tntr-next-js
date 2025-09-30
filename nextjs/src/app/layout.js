19a00fbc604c7cfebd300008d82fb9e6e38f5441
st4l <152074070+St4lV@users.noreply.github.com>
2025-09-30 14:39:45 +0200

    CSP correction

--- nextjs/src/app/layout.js
+++ nextjs/src/app/layout.js
@@ -1,34 +1,39 @@
 import "./globals.css";
 import Header from "./Header";
 import Footer from "./Footer";
-
-
+import { headers } from 'next/headers';
 import Context from "@/app/GlobalContext";
 
-export default function RootLayout({ children }) {
-	return (
-		<html lang="fr">
-		<head>
-			<meta charSet="UTF-8"/>
-			<title>Tirnatek Radio</title>
-			<meta
-			name="description"
-			content="Tekno Webradio basée en France, écoutez notre sélection de musiques tekno en direct, ainsi que les derniers DJ sets des artistes collaborant avec nous !"
-			/>
-			<link rel="preconnect" href="https://fonts.googleapis.com"/>
-			<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
-			<link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400..900&display=swap" rel="stylesheet"/>
-			<meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-			
-			<meta name="google-site-verification" content="cCnNinz1iS12t7Hg_AY0m9Zsl40wHLpis3uHDfrzH1Y" />
-		</head>
-		<body>
-			<Context>
-			<Header/>
-			{children}
-			<Footer/>
-			</Context>
-		</body>
-		</html>
-	);
+export default async function RootLayout({ children }) {
+   
+    const nonce = (await headers()).get('x-nonce');
+    
+    return (
+        <html lang="fr">
+        <head>
+            <meta charSet="UTF-8"/>
+            <title>Tirnatek Radio</title>
+            <meta
+                name="description"
+                content="Tekno Webradio basée en France, écoutez notre sélection de musiques tekno en direct, ainsi que les derniers DJ sets des artistes collaborant avec nous !"
+            />
+            <link rel="preconnect" href="https://fonts.googleapis.com" nonce={nonce}/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" nonce={nonce}/>
+            <link 
+                href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400..900&display=swap" 
+                rel="stylesheet"
+                nonce={nonce}
+            />
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <meta name="google-site-verification" content="cCnNinz1iS12t7Hg_AY0m9Zsl40wHLpis3uHDfrzH1Y" />
+        </head>
+        <body nonce={nonce}>
+            <Context>
+                <Header/>
+                {children}
+                <Footer/>
+            </Context>
+        </body>
+        </html>
+    );
 }
--- nextjs/src/middleware.js
+++ nextjs/src/middleware.js
@@ -1,65 +1,86 @@
 import { NextResponse } from 'next/server';
 
 export async function middleware(req) {
-	const url = new URL(req.url);
+    const url = new URL(req.url);
+   
+    //API PROXY
+    if (url.pathname.startsWith('/ap1/v1/')) {
+        const backendPath = url.pathname.replace('/ap1/v1', '/api/v1');
+        const backendUrl = `http://${process.env.BACKEND_ADRESS}:${process.env.BACKEND_PORT}${backendPath}${url.search}`;
+   
+        const res = await fetch(backendUrl, {
+            method: req.method,
+            headers: req.headers,
+            body: req.method !== 'GET' && req.method !== 'HEAD' ? await req.clone().arrayBuffer() : undefined,
+        });
+   
+        return new Response(res.body, {
+            status: res.status,
+            headers: res.headers,
+        });
+    }
+   
+    //Content Security Policy (CSP)
+    const nonce = Buffer.from(crypto.randomUUID()).toString('base64');
+    
+    let cspHeader;
+    
+    if (process.env.NODE_ENV === 'production') {
+        cspHeader = `
+            default-src 'self';
+            script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+            style-src 'self' 'nonce-${nonce}' 'unsafe-inline' 'unsafe-hashes' https://fonts.googleapis.com;
+            img-src 'self' blob: data:;
+            font-src 'self' https://fonts.gstatic.com;
+            object-src 'none';
+            base-uri 'self';
+            form-action 'self';
+            frame-ancestors 'none';
+            connect-src 'self';
+            upgrade-insecure-requests;
+        `;
+    } else {
+        cspHeader = `
+            default-src 'self';
+            script-src 'self' 'nonce-${nonce}' 'strict-dynamic' 'unsafe-eval';
+            style-src 'self' 'nonce-${nonce}' 'unsafe-inline' 'unsafe-hashes' https://fonts.googleapis.com;
+            img-src 'self' blob: data:;
+            font-src 'self' https://fonts.gstatic.com;
+            object-src 'none';
+            base-uri 'self';
+            form-action 'self';
+            frame-ancestors 'none';
+            connect-src 'self' ws: wss:;
+        `;
+    }
+   
+    const contentSecurityPolicyHeaderValue = cspHeader.replace(/\s{2,}/g, ' ').trim();
+   
+    const requestHeaders = new Headers(req.headers);
+    requestHeaders.set('x-nonce', nonce);
+   
+    const response = NextResponse.next({
+        request: {
+            headers: requestHeaders,
+        },
+    });
+   
+    response.headers.set(
+        'Content-Security-Policy',
+        contentSecurityPolicyHeaderValue
+    );
+   
+    return response;
+}
 
-	//PROXY API
-	if (url.pathname.startsWith('/ap1/v1/')) {
-		const backendPath = url.pathname.replace('/ap1/v1', '/api/v1');
-		const backendUrl = `http://${process.env.BACKEND_ADRESS}:${process.env.BACKEND_PORT}${backendPath}${url.search}`;
-
-		const res = await fetch(backendUrl, {
-			method: req.method,
-			headers: req.headers,
-			body: req.method !== 'GET' && req.method !== 'HEAD' ? await req.clone().arrayBuffer() : undefined,
-		});
-
-		return new Response(res.body, {
-
-		});
-	}
-
-	//Content Security Policy (CSP)
-	if (process.env.NODE_ENV === 'production') {
-		const nonce = Buffer.from(crypto.randomUUID()).toString('base64');
-		const cspHeader = `
-		default-src 'self';
-		script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
-		style-src 'self' 'nonce-${nonce}' 'unsafe-inline';
-		img-src 'self' blob: data:;
-		font-src 'self';
-		object-src 'none';
-		base-uri 'self';
-		form-action 'self';
-		frame-ancestors 'none';
-		connect-src 'self';
-		upgrade-insecure-requests;
-		`;
-
-		const contentSecurityPolicyHeaderValue = cspHeader.replace(/\s{2,}/g, ' ').trim();
-
-		const requestHeaders = new Headers(req.headers);
-		requestHeaders.set('x-nonce', nonce);
-
-		const response = NextResponse.next({
-			request: {headers: requestHeaders},
-		});
-
-		response.headers.set('Content-Security-Policy',contentSecurityPolicyHeaderValue);
-		return response;
-	}
-
-	return NextResponse.next();
-	}
-
-	export const config = {
-	matcher: [
-		{
-			source: '/((?!_next/static|_next/image|favicon.ico).*)',
-			missing: [
-				{ type: 'header', key: 'next-router-prefetch' },
-				{ type: 'header', key: 'purpose', value: 'prefetch' },
-			],
-		},
-	],
+export const config = {
+    matcher: [
+        {
+            source: '/((?!_next/static|_next/image|favicon.ico).*)',
+            missing: [
+                { type: 'header', key: 'next-router-prefetch' },
+                { type: 'header', key: 'purpose', value: 'prefetch' },
+            ],
+        },
+    ],
 };
